4c809b5e0e54168f0bab9f9935801d07f04c4f54
daihang <978093591@qq.com>
2019-04-14 12:17:06 +0800

    各类设计模式Demo初始化

---
+++ adapterModel/src/main/java/com/dh/AdapterMain.java
@@ -0,0 +1,7 @@
+package com.dh;
+
+public class AdapterMain {
+    public static void main(String[] args) {
+        System.out.println("初始化");
+    }
+}
---
+++ factoryModel/java/com.dh/FactoryMain.java
@@ -0,0 +1,9 @@
+package com.dh;
+
+
+public class FactoryMain {
+    public static void main(String[] args) {
+        System.out.println("初始化");
+    }
+
+}
